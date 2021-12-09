d5b51b5d9caee767fb05e80473325dda5aa2bd44
whatupcraig <bailey_craig@me.com>
2021-12-09 16:03:46 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,32 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'Operating System :: Microsoft :: Windows :: Windows 10',
-    'Operating System :: MacOS :: MacOS X',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3'
-]
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 setup(
-    name='firebotpy',
-    version='1.0',
-    description='Python library to connect to Firebots API',
-    url='https://github.com/whatupcraig/Firebotpy',
-    author='Craig Bailey',
-    author_email='bailey_craig@me.com',
-    license='MIT',
-    classifiers=classifiers,
-    keywords=['firebot', 'twitch bot', 'twitch', 'fire bot'],
-    packages=find_packages(),
-    install_requires=['requests']
+    name="firebotpy",
+    packages=["firebotpy"],
+    version="2.27",
+    license="MIT",
+    description="Python library to connect to Firebots API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="DadTheGam3r",
+    author_email="bailey_craig@me.com",
+    url="https://github.com/whatupcraig/Firebotpy",
+    download_url="https://github.com/whatupcraig/Firebotpy.git",
+    keywords=["firebot", "api", "twitch"],
+    install_requires=["requests"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",  
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
 )
