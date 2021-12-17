4b4bae3cd872ff4ee3833aa8f54c9ae610034c7b
whatupcraig <bailey_craig@me.com>
2021-12-16 22:20:33 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,31 @@
+This is a firebot library to connect to Firebots API. <br>
+
+```
+pip install firebotpy
+```
+
+https://firebot.app/
+From Firebots Website: <br>
+"Firebot is a fully featured open-source bot that can help level up your streams." <br>
+
+# Change Log
+
+## 2.29 (13/12/2021)
+
+-Added whisper chat from the bot or streamer account <br>
+-Added duration for custom variables (default is 0) <br>
+-Added getting the value of a counter <br>
+
+## 2.281 (12/12/2021)
+
+-Added set roles for users <br>
+-Added remove all users from roles <br>
+-Added enable and disable timer <br>
+-Added add and remove users to VIP roles <br>
+-Added ban and unban users <br>
+-Added change scene <br>
+-Fixed get_status function <br>
+
+## 2.26 (09/12/2021)
+
+-First Release <br>
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="firebotpy",
+    packages=["firebotpy"],
+    version="2.30",
+    license="MIT",
+    description="Python library to connect to Firebots API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="DadTheGam3r",
+    author_email="bailey_craig@me.com",
+    url="https://github.com/whatupcraig/Firebotpy",
+    download_url="https://github.com/whatupcraig/Firebotpy.git",
+    keywords=["firebot", "api", "twitch"],
+    install_requires=["requests"],
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",  # Define that your audience are developers
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
