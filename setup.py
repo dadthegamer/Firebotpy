e482fe52747300c5350ec378787718a4e0565933
whatupcraig <bailey_craig@me.com>
2022-01-07 17:27:27 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as f:
 setup(
     name="firebotpy",
     packages=["firebotpy"],
-    version="2.30",
+    version="2.32",
     license="MIT",
     description="Python library to connect to Firebots API",
     long_description=long_description,
